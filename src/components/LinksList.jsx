b5e3696e74a02af084a2f444f20c19eb0fa94327
lina98st <156576799+lina98st@users.noreply.github.com>
2026-01-02 12:29:45 +0100

    replaced portfolio website link

--- src/components/LinksList.jsx
+++ src/components/LinksList.jsx
@@ -1,7 +1,7 @@
 import LinkButton from "./LinkButton"
 
 const links = [ 
-   { label: "Portfolio", href: "https://lina98st.github.io/alina.dev/"},
+   { label: "Portfolio", href: "https://lina98st.github.io/portfolio-under-construction/"},
       {  label: "GitHub", href: "https://github.com/lina98st" },
        {     label: "LinkedIn", href: "https://www.linkedin.com/in/alina-schmi/"},
          {    label: "Hashnode", href: "https://alinatech.hashnode.dev/"},
