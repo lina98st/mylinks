50a0ed654be6ecd594054bece139bb865eba8e2e
lina98st <156576799+lina98st@users.noreply.github.com>
2026-03-07 14:39:04 +0100

    Update GitHub link

--- src/components/LinksList.jsx
+++ src/components/LinksList.jsx
@@ -2,7 +2,7 @@ import LinkButton from "./LinkButton"
 
 const links = [ 
    { label: "Portfolio", href: "https://lina98st.github.io/portfolio-under-construction/"},
-      {  label: "GitHub", href: "https://github.com/lina98st" },
+      {  label: "GitHub", href: "https://github.com/lina98st?tab=overview&from=2026-03-01&to=2026-03-07" },
        {     label: "LinkedIn", href: "https://www.linkedin.com/in/alina-schmi/"},
          {    label: "Hashnode", href: "https://alinatech.hashnode.dev/"},
 ]
